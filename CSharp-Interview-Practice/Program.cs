fa7dbda59ae2b258a8c994c56ec7c10d9b5e9269
smitakhutal <smitaborhade1990@gmail.com>
2025-08-29 01:33:53 +0530

    Reverse and palindrome logic optimize way to solve

--- CSharp-Interview-Practice/Program.cs
+++ CSharp-Interview-Practice/Program.cs
@@ -6,10 +6,76 @@ using System.Threading.Tasks;
 
 namespace CSharp_Interview_Practice
 {
-    internal class Program
+    internal class StringAndBasicProgramAccess
     {
         static void Main(string[] args)
         {
+            //Variable declaration
+            StringAndBasicProgramLogic logic = new StringAndBasicProgramLogic();
+            bool ContinueApp = true;
+            
+                Console.WriteLine("Provide input from given list");
+                Console.WriteLine("1.Reverse string without built-in Reverse()");
+                Console.WriteLine("2.Check palindrome string");
+                Console.WriteLine("3.Optimize version of two pointers for palindrome");
+                Console.WriteLine("4.Count vowels and consonants");
+                Console.WriteLine("5.Find first non-repeated character");
+                Console.WriteLine("0.Exit");
+            while (ContinueApp)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 0:
+                        ContinueApp=false;
+                        break;
+                    case 1:
+                        Console.WriteLine("Enter String to reverse");
+                        string str = Console.ReadLine();
+
+                        char[] arr = str.ToCharArray();
+
+                        string Result = logic.ReverseStringFunc(arr);
+                        Console.WriteLine($"{Result} string is reverse of {str}");
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter string to check");
+                        string strToCheck = Console.ReadLine();
+
+                        bool result = logic.PalindromeCheck(strToCheck);
+                        if (result)
+                        {
+                            Console.WriteLine($"{strToCheck} is Palindrome");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{strToCheck} is not Palindrome");
+                        }
+
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter stirng to check");
+                        string strToCheckPalindrome = Console.ReadLine();
+
+                        bool res = logic.CheckPalindrome(strToCheckPalindrome);
+                        if (res)
+                        {
+                            Console.WriteLine($"{strToCheckPalindrome} is Palindrome");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{strToCheckPalindrome} is not Palindrome");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("You have entered wrong input");
+                        break;
+
+                }
+            }
+            Console.WriteLine("Enter any keyto exit");
+            Console.ReadKey();
         }
+    
     }
 }
---
+++ CSharp-Interview-Practice/StringAndBasicProgramLogic.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Interview_Practice
+{
+    internal class StringAndBasicProgramLogic
+    {
+        public string ReverseStringFunc(char[] arr)
+        {
+            char[] res = new char[arr.Length];
+
+            for (int i = arr.Length - 1, j = 0; i >= 0; i--, j++)
+            {
+                res[j] = arr[i];
+            }
+            return new string(res);
+
+        }
+        public bool PalindromeCheck(string str)
+        {
+
+            string originalstring = str;
+            char[] str1 = str.ToCharArray();
+            int right = str.Length - 1, left = 0;
+            while (left < right)
+            {
+                char temp = str1[left];
+                str1[left] = str1[right];
+                str1[right] = temp;
+                right--;
+                left++;
+            }
+            if (new String(str1) == originalstring) return true;
+            else return false;
+
+
+        }
+
+        public  bool CheckPalindrome(string str)
+        {
+            int last = str.Length - 1, first = 0;
+
+            while (first < last)
+            {
+                if (str[first] != str[last])
+                    return false;
+
+                first++;
+                last--;
+            }
+
+            return true;
+        }
+
+    }
+}
