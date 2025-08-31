2868bdc2e7a33a978b42fb466b1f6da1c01adafc
smitakhutal <smitaborhade1990@gmail.com>
2025-08-31 14:53:01 +0530

    Count Vowels and consonants,find non-repeated characters,Remove Duplicate charactes,Find frequency of characters,Reverse words in a sentence,check anagram,print all substring and find longest word program in optimze way

--- CSharp-Interview-Practice/Program.cs
+++ CSharp-Interview-Practice/Program.cs
@@ -13,64 +13,196 @@ namespace CSharp_Interview_Practice
             //Variable declaration
             StringAndBasicProgramLogic logic = new StringAndBasicProgramLogic();
             bool ContinueApp = true;
-            
+            while (ContinueApp)
+            {
                 Console.WriteLine("Provide input from given list");
                 Console.WriteLine("1.Reverse string without built-in Reverse()");
                 Console.WriteLine("2.Check palindrome string");
                 Console.WriteLine("3.Optimize version of two pointers for palindrome");
                 Console.WriteLine("4.Count vowels and consonants");
                 Console.WriteLine("5.Find first non-repeated character");
-                Console.WriteLine("0.Exit");
-            while (ContinueApp)
-            {
-                int input = Convert.ToInt32(Console.ReadLine());
-                switch (input)
+                Console.WriteLine("6.Find first non-repeated character using Linq");
+                Console.WriteLine("7.Remove duplicate characters from a string");
+                Console.WriteLine("8.Remove duplicate characters from a string using linq");
+                Console.WriteLine("9.Find frequency of characters");
+                Console.WriteLine("10.Find frequency of characters using Linq");
+                Console.WriteLine("11.Reverse words in a sentence");
+                Console.WriteLine("12.Check anagrams");
+                Console.WriteLine("13.Print all substrings of a string");
+                Console.WriteLine("14.Find longest word in a string");
+                Console.WriteLine("0.Exit\n");
+                try
                 {
-                    case 0:
-                        ContinueApp=false;
-                        break;
-                    case 1:
-                        Console.WriteLine("Enter String to reverse");
-                        string str = Console.ReadLine();
+                    int input = Convert.ToInt32(Console.ReadLine());
+                    switch (input)
+                    {
+                        case 0:
+                            ContinueApp = false;
+                            break;
+                        case 1:
+                            Console.WriteLine("Enter String to reverse");
+                            string str = Console.ReadLine();
 
-                        char[] arr = str.ToCharArray();
+                            char[] arr = str.ToCharArray();
 
-                        string Result = logic.ReverseStringFunc(arr);
-                        Console.WriteLine($"{Result} string is reverse of {str}");
-                        break;
-                    case 2:
-                        Console.WriteLine("Enter string to check");
-                        string strToCheck = Console.ReadLine();
+                            string Result = logic.ReverseStringFunc(arr);
+                            Console.WriteLine($"{Result} string is reverse of {str}\n");
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter string to check");
+                            string strToCheck = Console.ReadLine();
 
-                        bool result = logic.PalindromeCheck(strToCheck);
-                        if (result)
-                        {
-                            Console.WriteLine($"{strToCheck} is Palindrome");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"{strToCheck} is not Palindrome");
-                        }
+                            bool result = logic.PalindromeCheck(strToCheck);
+                            if (result)
+                            {
+                                Console.WriteLine($"{strToCheck} is Palindrome \n");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{strToCheck} is not Palindrome\n");
+                            }
 
-                        break;
-                    case 3:
-                        Console.WriteLine("Enter stirng to check");
-                        string strToCheckPalindrome = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter stirng to check");
+                            string strToCheckPalindrome = Console.ReadLine();
 
-                        bool res = logic.CheckPalindrome(strToCheckPalindrome);
-                        if (res)
-                        {
-                            Console.WriteLine($"{strToCheckPalindrome} is Palindrome");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"{strToCheckPalindrome} is not Palindrome");
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("You have entered wrong input");
-                        break;
+                            bool res = logic.CheckPalindrome(strToCheckPalindrome);
+                            if (res)
+                            {
+                                Console.WriteLine($"{strToCheckPalindrome} is Palindrome \n");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{strToCheckPalindrome} is not Palindrome \n");
+                            }
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter string to count Vowels and Consonants");
+                            string vowelcheckstr = Console.ReadLine();
+                            int V_count = 0;
+                            int C_count = 0;
 
+                            logic.CountVowelsAndConsonants(vowelcheckstr, ref V_count, ref C_count);
+
+                            Console.WriteLine($"Vowels : {V_count} and Consonants : {C_count} \n");
+
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter string to check");
+                            string strForNonRepChar = Console.ReadLine();
+
+                            char? resultChar = logic.FirstNonRepeatChar(strForNonRepChar);
+
+                            if (resultChar.HasValue)
+                            {
+                                Console.WriteLine($"{resultChar} is first non-repeated character \n");
+                            }
+                            else
+                            {
+                                Console.WriteLine("All the characters are repeated \n");
+                            }
+                            break;
+                        case 6:
+                            Console.WriteLine("Enter string to check");
+                            string word = Console.ReadLine();
+                            logic.FirstNonRepeatCharLinqFunc(word);
+
+                            break;
+
+
+                        case 7:
+                            Console.WriteLine("Enter string to remove duplicate");
+                            string strToRemoveDup = Console.ReadLine();
+
+                            Console.WriteLine("Completely remove repeated characters from string");
+                            string resultStr = logic.RemoveDuplicateCharFunc(strToRemoveDup);
+
+                            Console.WriteLine($"{resultStr} after removing duplicate characters");
+
+                            Console.WriteLine("Remove character but keep first occurance from string");
+                            string resultstr1 = logic.RemoveDuplicateCharFunc1(strToRemoveDup);
+
+                            Console.WriteLine($"{resultstr1} after removing duplicate characters and keeping first occurance \n");
+
+                            break;
+                        case 8:
+                            Console.WriteLine("Enter String to remove Duplicate");
+                            string strToRemDupLinq = Console.ReadLine();
+
+                            logic.RemoveDuplicateCharLinqFunc(strToRemDupLinq);
+                            logic.RemoveDuplicateCharLinqFunc1(strToRemDupLinq);
+                            break;
+                        case 9:
+                            Console.WriteLine("Enter String to count frequency of character");
+                            string strToCountFreqChar = Console.ReadLine();
+
+                            Dictionary<char, int> resultFreqChar = new Dictionary<char, int>();
+
+                            resultFreqChar = logic.CountFrequencyCharFunc(strToCountFreqChar);
+
+                            foreach (var ch in resultFreqChar)
+                            {
+                                Console.WriteLine($"{ch.Key} : {ch.Value}");
+                            }
+
+
+                            break;
+                        case 10:
+                            Console.WriteLine("Enter string to check frequency of character");
+                            string strToCountFreqCharlinq = Console.ReadLine();
+                            logic.CountFrequencyCharLinqFunc(strToCountFreqCharlinq);
+
+                            break;
+                        case 11:
+                            Console.WriteLine("Enter string to reverse words");
+                            string strToReverseWords = Console.ReadLine();
+
+                            string ResultString = logic.ReverseWordFunc(strToReverseWords);
+
+                            Console.WriteLine($"String after reversing words:{ResultString} \n");
+                            break;
+                        case 12:
+                            Console.WriteLine("Enter first string");
+                            string first = Console.ReadLine();
+
+                            Console.WriteLine("Enter Second string");
+                            string second = Console.ReadLine();
+                            bool resOfAnagram = logic.CheckAnagrams(first, second);
+                            if (resOfAnagram)
+                            {
+                                Console.WriteLine("{0} and  {1} String are anagrams \n", first, second);
+                            }
+                            else
+                            {
+                                Console.WriteLine("{0} and  {1} String are not anagrams \n", first, second);
+                            }
+
+                            break;
+                        case 13:
+                            Console.WriteLine("Enter string to find substring");
+                            string strToFindSubstr = Console.ReadLine();
+
+                            logic.FindSubstringFunc(strToFindSubstr);
+
+                            break;
+                        case 14:
+                            Console.WriteLine("Enter string to find longest word");
+                            string strToFindLongWord = Console.ReadLine();
+
+                            string Longest = logic.LongestWordFunc(strToFindLongWord);
+                            Console.WriteLine(Longest + " is Longest word in string");
+
+                            break;
+                        default:
+                            Console.WriteLine("You have entered wrong input");
+                            break;
+
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
                 }
             }
             Console.WriteLine("Enter any keyto exit");
--- CSharp-Interview-Practice/StringAndBasicProgramLogic.cs
+++ CSharp-Interview-Practice/StringAndBasicProgramLogic.cs
@@ -54,6 +54,257 @@ namespace CSharp_Interview_Practice
 
             return true;
         }
+        public void CountVowelsAndConsonants(string str, ref int V_count, ref int C_count)
+        {
+            V_count = 0;
+            C_count = 0;
+            char[] arr = str.ToCharArray();
+
+            HashSet<char> vowels = new HashSet<char>(){ 'a', 'e', 'i', 'o', 'u'};
+
+            foreach (var ch in arr)
+            {
+                if (!Char.IsLetter(ch))
+                    continue;
+
+                if (vowels.Contains(Char.ToLower(ch)))
+                {
+                    V_count++;
+                }
+                else
+                {
+                    C_count++;
+                }
+            }
+
+        }
+
+        public char? FirstNonRepeatChar(string str)
+        {
+            Dictionary<char, int> freq = new Dictionary<char, int>();
+
+            foreach (var ch in str)
+            {
+                if (freq.ContainsKey(ch))
+                {
+                    freq[ch]++;
+                }
+                else
+                {
+                    freq[ch] = 1;
+                }
+            }
+
+            foreach (var ch in str)
+            {
+                if (freq[ch] == 1)
+                    return ch;
+            }
+            return null;
+
+        }
+        public string RemoveDuplicateCharFunc(string strToRemoveDup)
+        {
+            char[] word = strToRemoveDup.ToCharArray();
+            List<char> res = new List<char>();
+
+            Dictionary<char, int> freq = new Dictionary<char, int>();
+
+            foreach (var ch in word)
+            {
+                if (freq.ContainsKey(ch))
+                {
+                    freq[ch]++;
+                }
+                else
+                {
+                    freq[ch] = 1;
+                }
+            }
+            foreach (var ch in word)
+            {
+                if (freq[ch] == 1)
+                {
+                    res.Add(ch);
+                }
+            }
+            return new String(res.ToArray());
+
+        }
+        public string RemoveDuplicateCharFunc1(string str)
+        {
+            HashSet<char> check = new HashSet<char>();
+            List<char> result = new List<char>();
+
+            foreach (char ch in str)
+            {
+                if (!check.Contains(ch))
+                {
+                    check.Add(ch);
+                    result.Add(ch);
+                }
+            }
+            return new String(result.ToArray());
+        }
+        public void RemoveDuplicateCharLinqFunc(string strToRemDupLinq)
+        {
+            Console.WriteLine("Query Syntax");
+
+            var result = (from ch in strToRemDupLinq
+                          group ch by ch into g
+                          where g.Count() == 1
+                          select g.Key).ToArray();
+
+            Console.WriteLine($"{new string(result)}  after removing all occurances");
+
+
+            Console.WriteLine("Method Syntax");
+            var resultMethod = (strToRemDupLinq
+                 .GroupBy(c => c)
+                 .Where(g => g.Count() == 1)
+                                        .Select(g => g.Key)).ToArray();
+
+            Console.WriteLine($"{new string(resultMethod)} after removing all occurance\n");
+
+        }
+        public void RemoveDuplicateCharLinqFunc1(string strToRemDupLinq)
+        {
+            Console.WriteLine("Remove Duplicate by keeping first occurance");
+            Console.WriteLine("Query Syntax");
+
+            var KeepFirstOccur = (from ch in strToRemDupLinq
+                                  group ch by ch into g
+                                  select g.First()).ToArray();
+
+            Console.WriteLine($"{new string(KeepFirstOccur)} After removing duplicate characters by keeping first occurance\n");
+
+            var KeepFirstOccurMethod = strToRemDupLinq
+                                        .GroupBy(c => c)
+                                        .Select(g => g.First()).ToArray();
+
+            Console.WriteLine($"{new string(KeepFirstOccurMethod)} After removing duplicate characters by keeping first occurance\n");
+
+        }
+        public  Dictionary<char, int> CountFrequencyCharFunc(string str)
+        {
+            Dictionary<char, int> freq = new Dictionary<char, int>();
+
+            foreach (char ch in str)
+            {
+                if (freq.ContainsKey(ch))
+                {
+                    freq[ch]++;
+                }
+                else
+                {
+                    freq[ch] = 1;
+                }
+            }
+
+            return freq;
+        }
+        public void CountFrequencyCharLinqFunc(string strToCountFreqChar)
+        {
+            Console.WriteLine("Query Syntax");
+            var Freq = from ch in strToCountFreqChar
+                       group ch by ch into g
+                       select new { Character = g.Key, Count = g.Count() };
+
+            foreach (var item in Freq)
+            {
+                Console.WriteLine($"{item.Character}:{item.Count}");
+            }
+
+            Console.WriteLine("Method Syntax");
+
+            var FreqMethod = strToCountFreqChar
+                            .GroupBy(c => c)
+                            .Select(g => new { Character = g.Key, Count = g.Count() });
+
+            foreach (var item in FreqMethod)
+            {
+                Console.WriteLine($"{item.Character}:{item.Count} \n");
+            }
+
+        }
+        public  string ReverseWordFunc(string str)
+        {
+            string[] words = str.Split(' ');
+            int right = words.Length - 1, left = 0;
+
+            while (left < right)
+            {
+                string temp = words[left];
+                words[left] = words[right];
+                words[right] = temp;
+                left++;
+                right--;
+            }
+
+            return string.Join(" ", words);
+        }
+
+        public bool CheckAnagrams(string first,string second)
+        {
+            char[] FirstArr = first.ToLower().ToCharArray();
+            Array.Sort(FirstArr);
+            string FirstAfterSorting = new string(FirstArr);
+
+            char[] SecondArr = second.ToLower().ToCharArray();
+            Array.Sort(SecondArr);
+            string SecondAfterSorting = new string(SecondArr);
+
+            if (FirstAfterSorting == SecondAfterSorting) return true;
+            else return false;
+           
+        }
+
+        public void FindSubstringFunc(string str)
+        {
+            Console.WriteLine("Substrings are");
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = i; j < str.Length; j++)
+                {
+                    Console.WriteLine(str.Substring(i, j - i + 1));
+                }
+            }
+
+        }
+        public  string LongestWordFunc(string str)
+        {
+            string[] words = str.Split(' ');
+            string longestword = "";
+            foreach (var word in words)
+            {
+                if (word.Length > longestword.Length)
+                {
+                    longestword = word;
+                }
+            }
+            return longestword;
+        }
+
+        public void FirstNonRepeatCharLinqFunc(string word)
+        {
+            Console.WriteLine("Query Syntax");
+
+            var result = (from c in word
+                          group c by c into g
+                          where g.Count() == 1
+                          select g.Key).FirstOrDefault();
+            if (result != default(char))
+                Console.WriteLine($"{result} is first non repeated character\n");
+
+            Console.WriteLine("Method Syntax");
+
+            var result1 = word.GroupBy(c => c)
+                       .Where(g => g.Count() == 1)
+                       .Select(g=>g.Key).FirstOrDefault();
+            if (result1 != default(char))
+                Console.WriteLine($"{result1} is first non repeated  character\n");
+
+        }
 
     }
 }
